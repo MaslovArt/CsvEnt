a02c5c91fa851ffbab40ad7c9c4ddba1634dd193
Маслов Артем <artemiy_maslov@mail.ru>
2021-04-24 15:11:02 +0300

    Use inherited InnerException prop instead of custom

--- CsvEnt/Exceptions/CsvBindException.cs
+++ CsvEnt/Exceptions/CsvBindException.cs
@@ -5,16 +5,15 @@ namespace CsvEnt.Exceptions
     public class CsvBindException : Exception
     {
         public CsvBindException(string[] rowValues, int row, int col, Exception ex)
+            :base(ex.Message, ex)
         {
             RowValues = rowValues;
             Row = row;
             Col = col;
-            Inner = ex;
         }
 
         public int Row { get; set; }
         public int Col { get; set; }
         public string[] RowValues { get; set; }
-        public Exception Inner { get; set; }
     }
 }
